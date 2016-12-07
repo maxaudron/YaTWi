1507586a879167102a1e471e40ac3a7fdc19f628
Max Audron <audron@synmatter.com>
2016-12-07 13:10:19 +0100

    Modals now destroy on close
    fixes a ug were you could only send one message and had to reload the page to send another one

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -380,12 +380,16 @@ function message_dialog(clients2) {
       overlay: true,
       // Set if the modal can be closed by clicking on the overlay
       overlayClose: false,
+      'destroyOnClose': true,
       // Define a template to use for the modal
       template: '<div class="grid-block vertical"><div class="grid-content padding" style="padding-top: 1rem;"><h4 id="message_recipant">Send Message to: ' + clients2.client_nickname + '</h4><form name="messageform"><input id="message_mode" type="text" placeholder="mode" style="display: none" /><input id="message_target" type="text" style="display: none" /><input id="message_content" name="message" type="text" placeholder="Your Message" /><a ba-close="" class="button" onclick="send_message(this.form, &apos;1&apos;,' + clients2.clid + ')">Send</a><a ba-close="" class="button">Cancel</a></form></div></div>',
       // Allows you to pass in properties to the scope of the modal
-      contentScope: {
-        close: function() {
-          modal.destroy()
+      'contentScope': {
+        'close': function() {
+          modal.deactivate()
+          $timeout(function() {
+            modal.destroy()
+          }, 1000)
         }
       }
     })
@@ -404,12 +408,16 @@ function poke_dialog (clients2) {
       overlay: true,
       // Set if the modal can be closed by clicking on the overlay
       overlayClose: false,
+      'destroyOnClose': true,
       // Define a template to use for the modal
       template: '<div class="grid-block vertical"><div class="grid-content padding" style="padding-top: 1rem;"><h4 id="message_recipant">Poke: ' + clients2.client_nickname + '</h4><form name="messageform"><input id="message_mode" type="text" placeholder="mode" style="display: none" /><input id="message_target" type="text" style="display: none" /><input id="message_content" name="message" type="text" placeholder="Your Message" /><a ba-close="" class="button" onclick="send_poke(this.form,' + clients2.clid + ')">Send</a><a ba-close="" class="button">Cancel</a></form></div></div>',
       // Allows you to pass in properties to the scope of the modal
-      contentScope: {
-        close: function () {
-          modal.destr()
+      'contentScope': {
+        'close': function() {
+          modal.deactivate()
+          $timeout(function() {
+            modal.destroy()
+          }, 1000)
         }
       }
     })
