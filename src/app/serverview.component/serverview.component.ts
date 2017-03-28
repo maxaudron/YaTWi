1062d524446474b8255efc4ac4ca3dc2291c4d92
Max Audron <audron@synmatter.com>
2017-03-28 12:23:07 +0200

    Channel dislay correctly and spacers are a diferent color and stripped of their bbcode

--- src/app/serverview.component/serverview.component.ts
+++ src/app/serverview.component/serverview.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router }      from '@angular/router';
 import { ApiService, Data } from '../services/api.service';
 
+export class RegEx {}
+
 @Component({
   selector: 'serverview',
   templateUrl: './serverview.component.html',
@@ -12,10 +14,19 @@ export class ServerviewComponent {
     constructor(public apiService: ApiService) {}
     channels: Data[] = [];
     clients: Data[] = [];
+	regex: RegEx = '/(\[.*?spacer.*?\])+/';
 
     ngOnInit() { this.getData() }
     getData() {
         this.apiService.get('channellist').subscribe( response => this.channels = response )
         this.apiService.get('clientlist').subscribe( response => this.clients = response )
     }
+	regtest(channelname) {
+		var res = /(\[.*?spacer.*?\])+/g.test(channelname);
+		return res;
+	}
+	regstrip(channelname) {
+		var res = channelname.replace(/(\[.*?spacer.*?\])+/g, "");
+		return res
+	}
 }
